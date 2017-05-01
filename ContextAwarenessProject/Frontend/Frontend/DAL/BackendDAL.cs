51b2419ff11889d53aa3304ff6490f7e80ed433a
Jensschwee <Jensschwee@msn.com>
2017-05-01 15:42:11 +0200

    init frontend

---
+++ ContextAwarenessProject/Frontend/Frontend/DAL/BackendDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Frontend.Helpers;
+using Frontend.Model;
+using Newtonsoft.Json;
+
+namespace Frontend.DAL
+{
+    public class BackendDAL
+    {
+        public async Task<List<BoardInOut>> GetBoardData()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Backend.GetBackendBaseAdress() + "board/data");
+                HttpResponseMessage response = await client.GetAsync("");   
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var board = JsonConvert.DeserializeObject<List<BoardInOut>>(responseBody);
+                    if (board != null)
+                    {
+                        return board;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ ContextAwarenessProject/Frontend/Frontend/Model/BoardInOut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Model
+{
+    public class BoardInOut
+    {
+        public string deviceId { get; set; }
+        public string username { get; set; }
+        public string status { get; set; }
+        public string location { get; set; }
+        public string lastSeen { get; set; }
+    }
+}

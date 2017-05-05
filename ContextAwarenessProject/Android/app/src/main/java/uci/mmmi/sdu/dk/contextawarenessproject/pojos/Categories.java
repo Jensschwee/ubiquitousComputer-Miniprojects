b144b1da86204756f2b7e7554f05f1721af78593
Jensschwee <Jensschwee@msn.com>
2017-05-05 10:36:24 +0200

    init OU44 Location

---
+++ ContextAwarenessProject/Android/app/src/main/java/uci/mmmi/sdu/dk/contextawarenessproject/pojos/Categories.java
@@ -0,0 +1,21 @@
+package uci.mmmi.sdu.dk.contextawarenessproject.pojos;
+
+/**
+ * Created by jens on 05-05-2017.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+public class Categories {
+
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
